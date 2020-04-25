67c5df75b0a67b845781389f6aa58cca53257a16
Patrick Kerby <patrick@kerbydesign.com>
2020-04-24 23:16:01 -0600

    improve header navigation spacing. add logo

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -19,8 +19,7 @@ $sub_title = get_field('sub_title');
 @endphp
 
 <header class="banner">
-  <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+  <div class="container-fluid">
     <nav class="nav-primary">
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
