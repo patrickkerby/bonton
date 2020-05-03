3fba687d7febd523be054f4b48cea396fec68cf6
Patrick Kerby <patrick@kerbydesign.com>
2020-05-03 13:34:03 -0600

    Product modal close button responsive positioning

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -10,28 +10,26 @@ export default {
       $( this ).toggleClass( 'packed' );
     });
 
- // The following is to control the background overflow on body while a product modal is opened.
- // The close button that comes with the modal package is fucked, so we hide it via CSS, and make our own
-  
-  // add class to body when modal is triggered
-  $(document).ready(function() {
-    $(document).on('click', '.inside-thumb', function() {
-      $('body').addClass('modal-open');
+    // The following is to control the background overflow on body while a product modal is opened.
+    // The close button that comes with the modal package is fucked, so we hide it via CSS, and make our own
+    
+    // add class to body when modal is triggered
+    $(document).ready(function() {
+      $(document).on('click', '.inside-thumb', function() {
+        $('body').addClass('modal-open');
+      });
+      // remove class from body when close button is clicked  
+      $(document).on('click', '.close-product', function(e) {
+        if (!$(e.target).is('.modal-open'))
+          $('.modal-open').removeClass('modal-open');
+      });
+      // remove class from body when you click on the overlay
+      $(document).on('click', '.pp_overlay', function(e) {
+        if (!$(e.target).is('.modal-open'))
+          $('.modal-open').removeClass('modal-open');
+      });   
+      // close the modal when you click on our new button  
+      $('.close-product').on('click',function() { $.prettyPhoto.close(); });
     });
-    // remove class from body when close button is clicked  
-    $(document).on('click', '.close-product', function(e) {
-      if (!$(e.target).is('.modal-open'))
-        $('.modal-open').removeClass('modal-open');
-    });
-    // remove class from body when you click on the overlay
-    $(document).on('click', '.pp_overlay', function(e) {
-      if (!$(e.target).is('.modal-open'))
-        $('.modal-open').removeClass('modal-open');
-    });   
-    // close the modal when you click on our new button  
-    $('.close-product').on('click',function() { $.prettyPhoto.close(); });
-  });
-
-
   },
 };
