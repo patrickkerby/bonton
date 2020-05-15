a78bdc8120a14d04d219b063ca4087364cd20473
patrickkerby <patrick@kerbydesign.com>
2020-05-15 13:46:32 -0600

    Footer templating

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,19 @@
-<footer class="content-info">
-  <div class="container">
-    @php dynamic_sidebar('sidebar-footer') @endphp
+<footer class="container-fluid">
+  <div class="row">
+    <div class="col-sm4">
+      <div class="hours">
+        @foreach ($acf_options->hours_of_operation as $item)
+          <li>{{ $item->title }}: <span class="details">{{ $item->details }}</span></li>
+        @endforeach
+      </div>
+      <span class="phone">{{ $acf_options->phone }}</span>
+      <address>{{ $acf_options->address }}</address>
+    </div>
+    <div class="col-sm-4">
+      <img src="@asset('images/bonton-logo-no-border.svg')" />
+    </div>
+    <div class="col-sm-4">
+      @php dynamic_sidebar('sidebar-footer') @endphp
+    </div>
   </div>
 </footer>
