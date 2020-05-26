6baba88d3c15e37a91c30b70bb8962c93215a904
patrickkerby <patrick@kerbydesign.com>
2020-05-26 12:34:09 -0600

    Add Hot jar Tracking

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,6 +3,17 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="https://use.typekit.net/wvm4dbv.css">
+  <!-- Hotjar Tracking Code for https://bonton.workwithrye.ca -->
+<script>
+      (function(h,o,t,j,a,r){
+          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+          h._hjSettings={hjid:1829015,hjsv:6};
+          a=o.getElementsByTagName('head')[0];
+          r=o.createElement('script');r.async=1;
+          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+          a.appendChild(r);
+      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+  </script>
   @php wp_head() @endphp
   @if ( is_cart() )
     @php acf_form_head() @endphp
