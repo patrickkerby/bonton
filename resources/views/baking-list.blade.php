4ca63a34445167c0fcf030dab508e259038f669f
Patrick Kerby <patrick@kerbydesign.com>
2020-06-04 01:44:50 -0600

    Update baking list to use internal api

--- resources/views/baking-list.blade.php
+++ resources/views/baking-list.blade.php
@@ -5,86 +5,58 @@
 @extends('layouts.lists')
 
 @php
-$post_id = get_the_ID();
-do_action( 'acf/save_post', $post_id );
+  $post_id = get_the_ID();
+  do_action( 'acf/save_post', $post_id );
 
-use Automattic\WooCommerce\Client;
-use Automattic\WooCommerce\HttpClient\HttpClientException;
+  $date_selector_date = get_field('list_date');
 
-$site_url = home_url();
-$date_selector_date = get_field('pickup_date');
+// Get order data!
+  $query = new WC_Order_Query( array(  
+      'limit' => -1,
+      // 'orderby' => 'name',
+      // 'order' => 'asc',
+      'status' => 'processing',
+  ) );
+  $results = $query->get_orders();
 
-  $woocommerce = new Client(
-      $site_url, // Your store URL
-      'ck_a89cb8cd00b36072147bb5da86a500e16dc283d6', // Your consumer key
-      'cs_23e8504fe5f51b21b7385f1bc2e9c5b58f919d16', // Your consumer secret
-      [
-          'wp_api' => true, // Enable the WP REST API integration
-          'version' => 'wc/v3',
-          'verify_ssl' => false
-      ]
-  );
-
-  $results = $woocommerce->get('orders', array( 'status' => array('processing')));  
+//Create filtered list of orders based on the date selected on list page.
   $filtered_orders = array();
 
-  // Get date of each order, create array of only those arrays with meta value that matches the ACF variable
   foreach ( $results as $daily_results ) {    
-    $order_id = $daily_results->id;
-    $meta = $daily_results->meta_data;
-      
-    $approved_array = array(
-      'pickuplocation',
-      'pickup_date',
-      'pickup_timeslot'
-    );
-
-    $order_meta_arr = array(); // new list to store data that we can pull from 
-
-    foreach ($meta as $order_meta) {
-      if (!in_array($order_meta->key, $approved_array)) {
-            continue;
-        }
-        $order_meta_arr[$order_meta->key] = $order_meta->value;
-    }
-    
-    if ($order_meta_arr['pickup_date'] === $date_selector_date) {
+    $order_id = $daily_results->get_id();
+    $order_pickup_date = $daily_results->get_meta('pickup_date');
+          
+    if ($order_pickup_date === $date_selector_date) {
       $filtered_orders[] = $daily_results;
-    }    
+    }
   }
-  
-  
   
   foreach($filtered_orders as $details) {
 
-    foreach($details->line_items as $item) {
+    foreach ($details->get_items() as $item_id => $item) {
 
-      $prod_id = $item->product_id; 
-      $variation_id = $item->variation_id; 
-      
-      $product = $woocommerce->get('products/'.$prod_id.'');
-      $categories = $product->categories;
-      
-      $prefix = $cat_array = '';
-      foreach ($categories as $cat_item) 
-      {
-        $cat_names = $cat_item->name;
-        $cat_array .= $prefix . '' . $cat_names . '';
-        $prefix = ', ';
-      }
-            
+      $prod_id = $item->get_product_id(); 
+      $prod_quantity = $item->get_quantity();
+
+      $variation_id = $item->get_variation_id(); 
+
+      $product_raw = get_product($prod_id);
+      $product = $item->get_product($prod_id);
+      $prod_name = $product_raw->get_name();
+
+      $categories = $product->get_categories();
+      $variation_categories = $product_raw->get_categories();
+      $variation_categories_name = $variation_categories->name;
+
+      print_r($variation_categories_name);
+
       if (empty($variation_id)) {        
-        $prod[] = array('name' => $product->name, 'quantity' => $item->quantity, 'category' => $cat_array); 
+        $prod[] = array('name' => $prod_name, 'quantity' => $prod_quantity, 'category' => $categories); 
       }
       else {
-        $variations = $woocommerce->get('products/'.$prod_id.'/variations/'.$variation_id.'');
-        $variation_attributes = $variations->attributes;
-
-        foreach($variation_attributes as $variation) {
-          $variation_array = array_column($variation_attributes, 'option');          
-          $variation_list = implode(' - ', $variation_array);
-        }
-        $prod[] = array('name' => $product->name ." - " .$variation_list , 'quantity' => $item->quantity, 'category' => $cat_array); 
+        $variation_attributes = $product->get_attributes();
+        $option = $product->get_attribute( 'variety' );
+        $prod[] = array('name' => $prod_name ." - " .$option , 'quantity' => $prod_quantity, 'category' => $variation_categories); 
       }
     }
   }
@@ -120,53 +92,40 @@ $date_selector_date = get_field('pickup_date');
       return 1;
     }
   }
+@endphp
 
-  @endphp
-  @section('content')
+@section('content')
 
   <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-sm-10">
-        @php
-        acf_form(array(
-          'submit_value' => __('Choose Date', 'acf'),
-          'fields' => array(
-              'pickup_date',
-          ),
-          'return' => '%post_url%',
-          'updated_message' => false,
-        ));
-        @endphp
-      </div>
-    </div>
-  <div class="row no-gutters">
-    <table id="lists" class="display">
-      <thead>
-        <tr>
-          <th>Product</th>
-          <th>Category</th>
-          <th>Pkg. Quantity</th>
-          <th>Total Items</th>
-        </tr>
-      </thead>
-      <tbody>
-        @foreach ($aSortedArray as $item )
-          @php 
-            $name = $item['name'];
-            $category = $item['category'];
-            $quantity = $item['quantity'];                      
-            $item_quantity = call_user_func('itemQuantity', $name);
-            $total_items = $item_quantity * $quantity;  
-          @endphp
+    <div class="row no-gutters">
+      <table id="lists" class="display">
+        <thead>
           <tr>
-            <td>{{ $name }}</td>
-            <td>{{ $category }}</td>
-            <td>{{ $quantity }}</td>
-            <td>{{ $total_items }}</td>
+            <th>Product</th>
+            <th>Category</th>
+            <th>Pkg. Quantity</th>
+            <th>Total Items</th>
           </tr>
-        @endforeach
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          @foreach ($aSortedArray as $item )
+            @php 
+              $name = $item['name'];
+              $category = $item['category'];
+              $quantity = $item['quantity'];                      
+              $item_quantity = call_user_func('itemQuantity', $name);
+              $total_items = $item_quantity * $quantity;  
+            @endphp
+            <tr>
+              <td>{{ $name }}</td>
+              <td>{!! $category !!}</td>
+              <td>{{ $quantity }}</td>
+              <td>{{ $total_items }}</td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
   </div>
 
 @endsection
