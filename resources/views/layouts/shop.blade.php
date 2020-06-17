809b7ee6aeb6a8405c38bda24e8870b5abbfddfc
Patrick Kerby <patrick@kerbydesign.com>
2020-06-16 22:14:10 -0600

    Add product filters to mobile

--- resources/views/layouts/shop.blade.php
+++ resources/views/layouts/shop.blade.php
@@ -8,17 +8,22 @@
     <div class="wrap container-fluid" role="document">
       @if (App\display_sidebar())
         <main class="main row justify-content-center">
+          <button class="navbar-toggler mobile-filter collapsed" type="button" data-toggle="collapse" data-target="#mobileFilter" aria-controls="mobile-filter" aria-expanded="false" aria-label="Toggle navigation">Filter Products</button>
+          <div id="mobileFilter" class="sidebar sidebar-mobile collapse">
+            <button class="navbar-toggler mobile-filter" type="button" data-toggle="collapse" data-target="#mobileFilter" aria-controls="mobile-filter" aria-expanded="false" aria-label="Toggle navigation">Close</button>
+            @include('partials.sidebar')
+          </div>  
           <div class="sidebar typetura d-none d-sm-block col-sm-4 col-lg-3">
             @include('partials.sidebar')
           </div>
           <div class="content col-sm-8 col-lg-9">
             @php dynamic_sidebar('sidebar-shop') @endphp
             @yield('content')
-          </div>                    
+          </div>
         </main>
       @else
         <main class="main row">
-          <div class="content col-sm-11">                  
+          <div class="content col-sm-11">
             @yield('content')
           </div>
         </main>
