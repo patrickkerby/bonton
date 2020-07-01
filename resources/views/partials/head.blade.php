c62d68fcfe9322df9a4e71f6cdba28629821528a
Patrick Kerby <patrick@kerbydesign.com>
2020-07-01 08:30:45 -0600

    Update sidebar styles due to plugin update

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,7 +3,6 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   {{-- <meta http-equiv="Content-Security-Policy" content="default-src https: 'unsafe-inline' 'unsafe-eval'; worker-src blob: ; child-src blob: ; img-src data: blob: ;"> --}}
-  <link rel="stylesheet" href="https://use.typekit.net/wvm4dbv.css">
   <!-- Hotjar Tracking Code for https://bonton.workwithrye.ca -->
 {{-- <script>
       (function(h,o,t,j,a,r){
@@ -24,6 +23,6 @@
   @endif
   
   @php wp_head() @endphp
-
+  <link rel="stylesheet" rel="preload" href="https://use.typekit.net/wvm4dbv.css" crossorigin>
 
 </head>
