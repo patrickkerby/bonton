3bde377d2b77aa70e567aa649a7e107bc8532c2b
patrickkerby <patrick@kerbydesign.com>
2020-07-13 12:22:46 -0600

    add LF notice on cart page

--- resources/views/woocommerce/cart/cart.blade.php
+++ resources/views/woocommerce/cart/cart.blade.php
@@ -66,8 +66,191 @@ defined( 'ABSPATH' ) || exit;
 @endphp
 
 
-	<div class="row justify-content-center">
-		<div class='col-sm-4'>
+	<div class="row justify-content-center">	
+		<div class="col-md-8">
+			<form class="woocommerce-cart-form" action="<?php echo esc_url( wc_get_cart_url() ); ?>" method="post">
+				<?php do_action( 'woocommerce_before_cart_table' ); ?>
+				<table class="shop_table cart woocommerce-cart-form__contents" cellspacing="0">
+					<thead>
+						<tr>
+							<th class="product-remove">&nbsp;</th>
+							<th class="product-name"><?php esc_html_e( 'Product', 'woocommerce' ); ?></th>
+							<th class="product-price"><?php esc_html_e( 'Price', 'woocommerce' ); ?></th>
+							<th class="product-quantity"><?php esc_html_e( 'Quantity', 'woocommerce' ); ?></th>
+							<th class="product-subtotal"><?php esc_html_e( 'Subtotal', 'woocommerce' ); ?></th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php do_action( 'woocommerce_before_cart_contents' ); ?>
+
+						<?php
+						foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
+							$_product   = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
+							$product_id = apply_filters( 'woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key );
+
+							if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
+								$product_permalink = apply_filters( 'woocommerce_cart_item_permalink', $_product->is_visible() ? $_product->get_permalink( $cart_item ) : '', $cart_item, $cart_item_key );
+								?>
+
+								<?php
+								// Check availability
+								$availability = get_field('availability', $product_id );
+
+								$prefix = $days_available = '';
+								if (is_array($availability) || is_object($availability)) {
+										
+										foreach ($availability as $term) {
+												$days = $term->name;
+												$days_available .= $prefix . '' . $days . '';
+												$prefix = ', ';
+										}
+									}
+									$days_available = explode(", ",$days_available);
+
+									if(!in_array($day_of_week, $days_available)){
+										$availability_status = "not-available";
+										$availability_msg = '<span class="not-available-message">This product is not available on your selected pickup date!<br> Please remove, or select different pickup date.</span>';
+									}
+									else {
+										$availability_msg = "";
+										$availability_status = "available";
+									}
+
+									//Check if requires long fermentation lead time
+									if ( has_term( array('long-fermentation'), 'product_tag', $product_id ) ){
+										$long_fermentation = True;
+									}
+									
+								?>
+							
+							<tr class="<?php echo $availability_status; ?> title woocommerce-cart-form__cart-item <?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
+								<td class="product-remove">
+									<?php
+										echo apply_filters( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+											'woocommerce_cart_item_remove_link',
+											sprintf(
+												'<a href="%s" class="remove" aria-label="%s" data-product_id="%s" data-product_sku="%s">&times;</a>',
+												esc_url( wc_get_cart_remove_url( $cart_item_key ) ),
+												esc_html__( 'Remove this item', 'woocommerce' ),
+												esc_attr( $product_id ),
+												esc_attr( $_product->get_sku() )
+											),
+											$cart_item_key
+										);
+									?>
+								</td>	
+								<td class="product-name" colspan="4" data-title="<?php esc_attr_e( 'Product', 'woocommerce' ); ?>">
+									@php
+									if ( ! $product_permalink ) {
+										echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key ) . '&nbsp;' );
+									} else {
+										echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', sprintf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $_product->get_name() ), $cart_item, $cart_item_key ) );
+									}
+
+									do_action( 'woocommerce_after_cart_item_name', $cart_item, $cart_item_key );								
+
+									@endphp
+								</td>
+							</tr>
+							
+							<tr class="<?php echo $availability_status; ?> woocommerce-cart-form__cart-item <?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
+								<td></td>
+								<td class="product-meta" data-title="<?php esc_attr_e( 'Product', 'woocommerce' ); ?>">
+									<?php
+									// Meta data.
+									echo wc_get_formatted_cart_item_data( $cart_item ); // PHPCS: XSS ok.
+
+									// Backorder notification.
+									if ( $_product->backorders_require_notification() && $_product->is_on_backorder( $cart_item['quantity'] ) ) {
+										echo wp_kses_post( apply_filters( 'woocommerce_cart_item_backorder_notification', '<p class="backorder_notification">' . esc_html__( 'Available on backorder', 'woocommerce' ) . '</p>', $product_id ) );
+									}
+									?>
+
+									<?php
+										if (in_array('Everyday', $days_available)) {
+											echo '<span class="availability"><strong>Availability: </strong> Everyday!</span>';
+										}
+										else {
+												$days = implode(', ', $days_available);
+												echo '<span class="availability"><strong>Availability: </strong>' . $days . '</span>';
+										}
+									?>				
+									</td>
+
+									<td class="product-price" data-title="<?php esc_attr_e( 'Price', 'woocommerce' ); ?>">
+										<?php
+											echo apply_filters( 'woocommerce_cart_item_price', WC()->cart->get_product_price( $_product ), $cart_item, $cart_item_key ); // PHPCS: XSS ok.
+										?>
+									</td>
+
+									<td class="product-quantity" data-title="<?php esc_attr_e( 'Quantity', 'woocommerce' ); ?>">
+									<?php
+									if ( $_product->is_sold_individually() ) {
+										$product_quantity = sprintf( '1 <input type="hidden" name="cart[%s][qty]" value="1" />', $cart_item_key );
+									} else {
+										$product_quantity = woocommerce_quantity_input(
+											array(
+												'input_name'   => "cart[{$cart_item_key}][qty]",
+												'input_value'  => $cart_item['quantity'],
+												'max_value'    => $_product->get_max_purchase_quantity(),
+												'min_value'    => '0',
+												'product_name' => $_product->get_name(),
+											),
+											$_product,
+											false
+										);
+									}
+
+									echo apply_filters( 'woocommerce_cart_item_quantity', $product_quantity, $cart_item_key, $cart_item ); // PHPCS: XSS ok.
+									?>
+									</td>
+
+									<td class="product-subtotal" data-title="<?php esc_attr_e( 'Subtotal', 'woocommerce' ); ?>">
+										<?php
+											echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key ); // PHPCS: XSS ok.
+										?>
+									</td>
+								</tr>
+								@if($availability_msg)
+									<tr class="not-available">
+										<td colspan="5">{!! $availability_msg !!}</td>
+									</tr>
+									@php static $conflict = true; @endphp
+								@else
+
+								@endif							
+								<?php
+							}
+						}
+						?>
+
+						<?php do_action( 'woocommerce_cart_contents' ); ?>
+
+						<tr>
+							<td colspan="6" class="actions">
+
+								<?php if ( wc_coupons_enabled() ) { ?>
+									<div class="coupon">
+										<label for="coupon_code"><?php esc_html_e( 'Coupon:', 'woocommerce' ); ?></label> <input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e( 'Coupon code', 'woocommerce' ); ?>" /> <button type="submit" class="button" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>"><?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?></button>
+										<?php do_action( 'woocommerce_cart_coupon' ); ?>
+									</div>
+								<?php } ?>
+
+								<button type="submit" class="button" name="update_cart" value="<?php esc_attr_e( 'Update cart', 'woocommerce' ); ?>"><?php esc_html_e( 'Update cart', 'woocommerce' ); ?></button>
+
+								<?php do_action( 'woocommerce_cart_actions' ); ?>
+
+								<?php wp_nonce_field( 'woocommerce-cart', 'woocommerce-cart-nonce' ); ?>
+							</td>
+						</tr>
+
+						<?php do_action( 'woocommerce_after_cart_contents' ); ?>
+					</tbody>
+				</table>
+				<?php do_action( 'woocommerce_after_cart_table' ); ?>
+			</form>
+		</div>
+		<div class='col-sm-4 order-first'>
 			<form method="post" class="acf-form">
 				<div class="">
 					<div class="acf-field acf-field-date-picker">
@@ -113,206 +296,27 @@ defined( 'ABSPATH' ) || exit;
 					</div>
 				</div>
 			</form>
-		
-		</div>
-	
-		<div class="col-md-8">
-		<form class="woocommerce-cart-form" action="<?php echo esc_url( wc_get_cart_url() ); ?>" method="post">
-			<?php do_action( 'woocommerce_before_cart_table' ); ?>
-			<table class="shop_table cart woocommerce-cart-form__contents" cellspacing="0">
-				<thead>
-					<tr>
-						<th class="product-remove">&nbsp;</th>
-						<th class="product-name"><?php esc_html_e( 'Product', 'woocommerce' ); ?></th>
-						<th class="product-price"><?php esc_html_e( 'Price', 'woocommerce' ); ?></th>
-						<th class="product-quantity"><?php esc_html_e( 'Quantity', 'woocommerce' ); ?></th>
-						<th class="product-subtotal"><?php esc_html_e( 'Subtotal', 'woocommerce' ); ?></th>
-					</tr>
-				</thead>
-				<tbody>
-					<?php do_action( 'woocommerce_before_cart_contents' ); ?>
-
-					<?php
-					foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
-						$_product   = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
-						$product_id = apply_filters( 'woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key );
-
-						if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
-							$product_permalink = apply_filters( 'woocommerce_cart_item_permalink', $_product->is_visible() ? $_product->get_permalink( $cart_item ) : '', $cart_item, $cart_item_key );
-							?>
-
-							<?php
-							// Check availability
-							$availability = get_field('availability', $product_id );
-
-							$prefix = $days_available = '';
-							if (is_array($availability) || is_object($availability)) {
-									
-									foreach ($availability as $term) {
-											$days = $term->name;
-											$days_available .= $prefix . '' . $days . '';
-											$prefix = ', ';
-									}
-								}
-								$days_available = explode(", ",$days_available);
-
-								if(!in_array($day_of_week, $days_available)){
-									$availability_status = "not-available";
-									$availability_msg = '<span class="not-available-message">This product is not available on your selected pickup date!<br> Please remove, or select different pickup date.</span>';
-								}
-								else {
-									$availability_msg = "";
-									$availability_status = "available";
-								}
-
-								//Check if requires long fermentation lead time
-								if ( has_term( array('long-fermentation'), 'product_tag', $product_id ) ){
-									$long_fermentation = True;
-								}
-								
-							?>
-						
-						<tr class="<?php echo $availability_status; ?> title woocommerce-cart-form__cart-item <?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
-							<td class="product-remove">
-								<?php
-									echo apply_filters( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-										'woocommerce_cart_item_remove_link',
-										sprintf(
-											'<a href="%s" class="remove" aria-label="%s" data-product_id="%s" data-product_sku="%s">&times;</a>',
-											esc_url( wc_get_cart_remove_url( $cart_item_key ) ),
-											esc_html__( 'Remove this item', 'woocommerce' ),
-											esc_attr( $product_id ),
-											esc_attr( $_product->get_sku() )
-										),
-										$cart_item_key
-									);
-								?>
-							</td>	
-							<td class="product-name" colspan="4" data-title="<?php esc_attr_e( 'Product', 'woocommerce' ); ?>">
-								@php
-								if ( ! $product_permalink ) {
-									echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key ) . '&nbsp;' );
-								} else {
-									echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', sprintf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $_product->get_name() ), $cart_item, $cart_item_key ) );
-								}
-
-								do_action( 'woocommerce_after_cart_item_name', $cart_item, $cart_item_key );								
-
-								@endphp
-							</td>
-						</tr>
-						
-						<tr class="<?php echo $availability_status; ?> woocommerce-cart-form__cart-item <?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
-							<td></td>
-							<td class="product-meta" data-title="<?php esc_attr_e( 'Product', 'woocommerce' ); ?>">
-								<?php
-								// Meta data.
-								echo wc_get_formatted_cart_item_data( $cart_item ); // PHPCS: XSS ok.
-
-								// Backorder notification.
-								if ( $_product->backorders_require_notification() && $_product->is_on_backorder( $cart_item['quantity'] ) ) {
-									echo wp_kses_post( apply_filters( 'woocommerce_cart_item_backorder_notification', '<p class="backorder_notification">' . esc_html__( 'Available on backorder', 'woocommerce' ) . '</p>', $product_id ) );
-								}
-								?>
-
-								<?php
-									if (in_array('Everyday', $days_available)) {
-										echo '<span class="availability"><strong>Availability: </strong> Everyday!</span>';
-									}
-									else {
-											$days = implode(', ', $days_available);
-											echo '<span class="availability"><strong>Availability: </strong>' . $days . '</span>';
-									}
-								?>				
-								</td>
-
-								<td class="product-price" data-title="<?php esc_attr_e( 'Price', 'woocommerce' ); ?>">
-									<?php
-										echo apply_filters( 'woocommerce_cart_item_price', WC()->cart->get_product_price( $_product ), $cart_item, $cart_item_key ); // PHPCS: XSS ok.
-									?>
-								</td>
-
-								<td class="product-quantity" data-title="<?php esc_attr_e( 'Quantity', 'woocommerce' ); ?>">
-								<?php
-								if ( $_product->is_sold_individually() ) {
-									$product_quantity = sprintf( '1 <input type="hidden" name="cart[%s][qty]" value="1" />', $cart_item_key );
-								} else {
-									$product_quantity = woocommerce_quantity_input(
-										array(
-											'input_name'   => "cart[{$cart_item_key}][qty]",
-											'input_value'  => $cart_item['quantity'],
-											'max_value'    => $_product->get_max_purchase_quantity(),
-											'min_value'    => '0',
-											'product_name' => $_product->get_name(),
-										),
-										$_product,
-										false
-									);
-								}
-
-								echo apply_filters( 'woocommerce_cart_item_quantity', $product_quantity, $cart_item_key, $cart_item ); // PHPCS: XSS ok.
-								?>
-								</td>
-
-								<td class="product-subtotal" data-title="<?php esc_attr_e( 'Subtotal', 'woocommerce' ); ?>">
-									<?php
-										echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key ); // PHPCS: XSS ok.
-									?>
-								</td>
-							</tr>
-							@if($availability_msg)
-								<tr class="not-available">
-									<td colspan="5">{!! $availability_msg !!}</td>
-								</tr>
-								@php static $conflict = true; @endphp
-							@else
-
-							@endif							
-							<?php
-						}
-					}
-					?>
-
-					<?php do_action( 'woocommerce_cart_contents' ); ?>
-
-					<tr>
-						<td colspan="6" class="actions">
-
-							<?php if ( wc_coupons_enabled() ) { ?>
-								<div class="coupon">
-									<label for="coupon_code"><?php esc_html_e( 'Coupon:', 'woocommerce' ); ?></label> <input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e( 'Coupon code', 'woocommerce' ); ?>" /> <button type="submit" class="button" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>"><?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?></button>
-									<?php do_action( 'woocommerce_cart_coupon' ); ?>
-								</div>
-							<?php } ?>
-
-							<button type="submit" class="button" name="update_cart" value="<?php esc_attr_e( 'Update cart', 'woocommerce' ); ?>"><?php esc_html_e( 'Update cart', 'woocommerce' ); ?></button>
-
-							<?php do_action( 'woocommerce_cart_actions' ); ?>
-
-							<?php wp_nonce_field( 'woocommerce-cart', 'woocommerce-cart-nonce' ); ?>
-						</td>
-					</tr>
-
-					<?php do_action( 'woocommerce_after_cart_contents' ); ?>
-				</tbody>
-			</table>
-			<?php do_action( 'woocommerce_after_cart_table' ); ?>
-		</form>
+				@if ( $long_fermentation == True)
+					<div class="lf_notice"> 
+						<strong>Note:</strong> Next-day pickup is unavailable for Sourdough breads (They need 40 hours of fermentation).
+					</div>
+				@endif
 		</div>
 
-	<?php do_action( 'woocommerce_before_cart_collaterals' ); ?>
-
-	<div class="cart-collaterals @isset($conflict) conflict @endisset col-sm-12">
-		<?php
-			/**
-			 * Cart collaterals hook.
-			 *
-			 * @hooked woocommerce_cross_sell_display
-			 * @hooked woocommerce_cart_totals - 10
-			 */
-			do_action( 'woocommerce_cart_collaterals' );
-		?>
 	</div>
+		<?php do_action( 'woocommerce_before_cart_collaterals' ); ?>
+
+		<div class="cart-collaterals @isset($conflict) conflict @endisset col-sm-12">
+			<?php
+				/**
+				 * Cart collaterals hook.
+				 *
+				 * @hooked woocommerce_cross_sell_display
+				 * @hooked woocommerce_cart_totals - 10
+				 */
+				do_action( 'woocommerce_cart_collaterals' );
+			?>
+		</div>
 
 <?php do_action( 'woocommerce_after_cart' ); ?>
 <script>
@@ -347,8 +351,6 @@ defined( 'ABSPATH' ) || exit;
 				// $( "#datepicker" ).datepicker( "option", "defaultDate", +2 );
 				$( "#datepicker" ).datepicker( "option", "dateFormat", "DD, MM d, yy" );
 				$( "#datepicker" ).datepicker( "option", "showButtonPanel", true );
-
-
 			});
 		});
 	});
