71b5a0ab77d19c56834c41cbdae13f7d99c53ca1
Patrick Kerby <patrick@kerbydesign.com>
2020-12-10 12:10:22 -0700

    check again

--- resources/views/inventory-list.blade.php
+++ resources/views/inventory-list.blade.php
@@ -138,7 +138,7 @@ foreach ($date_range as $day) {
   $listedProducts = array_column($productsPerDay, 'name');
   $uniqueListedProducts = array_unique($listedProducts);
 
-  print("<pre>".print_r($dailyProducts,true)."</pre>");
+  print("<pre>".print_r($prod,true)."</pre>");
   // print("<pre>".print_r($uniqueListedProducts,true)."</pre>");
 
 @endphp
