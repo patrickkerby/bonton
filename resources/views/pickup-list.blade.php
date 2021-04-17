424ec2316e8fb79c7c590d42c12f8c7c461bbc2d
Patrick Kerby <patrick@kerbydesign.com>
2021-04-17 07:34:50 -0600

    add completed orders to pickup list

--- resources/views/pickup-list.blade.php
+++ resources/views/pickup-list.blade.php
@@ -15,7 +15,7 @@
       'limit' => -1,
       // 'orderby' => 'name',
       // 'order' => 'asc',
-      'status' => 'processing',
+      'status' => array('wc-processing', 'wc-completed'),
   ) );
   $results = $query->get_orders();
 
