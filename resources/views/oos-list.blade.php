8a261a561ef7b16d810be1608d9901b80d6953bd
Patrick Kerby <patrick@kerbydesign.com>
2021-10-02 09:27:27 -0400

    OOS testing

--- resources/views/oos-list.blade.php
+++ resources/views/oos-list.blade.php
@@ -24,7 +24,7 @@
     // $query = new WP_Query($query_args);
     $prodlist = get_posts( $query_args );
 
-    print("<pre>".print_r($array_data,true)."</pre>");
+    print("<pre>".print_r($prodlist,true)."</pre>");
 
 
 @endphp
