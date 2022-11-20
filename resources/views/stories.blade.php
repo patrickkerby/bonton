7b528e3e1883637fc89e4c845ee4f8b17cf14402
Patrick Kerby <patrick@kerbydesign.com>
2022-11-20 13:00:51 -0700

    Add modals to story pages

--- resources/views/stories.blade.php
+++ resources/views/stories.blade.php
@@ -35,7 +35,7 @@
         </div>
         @if($item['modal'])
         <div class="modal fade" id="storyModal{{ $loop->iteration }}" tabindex="-1" role="dialog" aria-labelledby="storyModalLabel{{ $loop->iteration }}" aria-hidden="true">
-          <div class="modal-dialog" role="document">
+          <div class="modal-dialog modal-dialog-centered modal-sm" role="document">
             <div class="modal-content">
               <div class="modal-body">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
