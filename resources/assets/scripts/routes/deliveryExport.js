06db26beafadc3694a5549e10463b8aa432c12f6
Patrick Kerby <patrick@kerbydesign.com>
2023-02-24 16:30:25 -0700

    update deeleeo

--- resources/assets/scripts/routes/deliveryExport.js
+++ resources/assets/scripts/routes/deliveryExport.js
@@ -78,7 +78,7 @@ export default {
             {
                 'extend': 'csv',
                 'exportOptions': {
-                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
                 },
             },
           ],
