6143d26e2afd1a135d305fcf98e78410a53e037c
Patrick Kerby <patrick@kerbydesign.com>
2023-02-27 16:02:20 -0700

    update deeleeo

--- resources/assets/scripts/routes/deliveryExport.js
+++ resources/assets/scripts/routes/deliveryExport.js
@@ -19,7 +19,7 @@ export default {
           'dom': 'Bfrtip',
           'buttons': [
             {
-                'extend': 'csv',
+                'extend': 'excel', 
                 'exportOptions': {
                     columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
                 },
