535a2f89f7bb3886d8183f4013ccea481d600a74
Patrick Kerby <patrick@kerbydesign.com>
2023-07-04 11:04:16 -0600

    add gst to fee

--- app/filters.php
+++ app/filters.php
@@ -861,7 +861,7 @@ function delivery_bag_fee() {
 	$chosen_shipping_method = WC()->session->get('chosen_shipping_methods'); 
 
 	if (strpos($chosen_shipping_method[0], 'flat_rate') !== false) {
-		WC()->cart->add_fee(__('Delivery Bag Fee', 'txtdomain'), 0.3, true);
+		WC()->cart->add_fee(__('Delivery Bag Fee (includes tax)', 'txtdomain'), 0.3, true);
 	}
 }
 
