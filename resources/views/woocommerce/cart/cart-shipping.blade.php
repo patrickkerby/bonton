f0b2ea3180920f1cdd960c6de0adb7e59ebfda8d
Patrick Kerby <patrick@kerbydesign.com>
2023-11-18 09:49:20 -0500

    update pickup date for dec 23

--- resources/views/woocommerce/cart/cart-shipping.blade.php
+++ resources/views/woocommerce/cart/cart-shipping.blade.php
@@ -47,7 +47,7 @@ if($session_date_object) {
 	$pickup_day_of_week = $session_date_object->format('l');
 	$pickup_date = $session_date_object->format('Y-m-d');
 
-	if ($pickup_day_of_week === "Saturday" && $pickup_date != "2022-08-13" && !$icecream_conflict) {
+	if ($pickup_day_of_week === "Saturday" && $pickup_date != "2022-12-23" && !$icecream_conflict) {
 		$delivery_available = true;
 	}
 	elseif ($is_wholesale_user) {
