1bda5716308d933147ce41ff681b66883fb3d53d
Patrick Kerby <patrick@kerbydesign.com>
2026-07-30 16:59:51 -0600

    email design fixes

---
+++ resources/views/woocommerce/emails/email-footer.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Email Footer
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/emails/email-footer.php.
+ *
+ * @see https://woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates\Emails
+ * @version 10.0.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+$email = $email ?? null;
+$bg    = get_option( 'woocommerce_email_background_color', '#fcfcfc' );
+$body  = get_option( 'woocommerce_email_body_background_color', '#ffffff' );
+
+?>
+																		</div>
+																	</td>
+																</tr>
+															</table>
+															<!-- End Content -->
+														</td>
+													</tr>
+												</table>
+												<!-- End Body -->
+											</td>
+										</tr>
+									</table>
+								</td>
+							</tr>
+							<tr>
+								<td align="center" valign="top" bgcolor="<?php echo esc_attr( $bg ); ?>" style="background-color:<?php echo esc_attr( $bg ); ?>;">
+									<!-- Footer -->
+									<table border="0" cellpadding="10" cellspacing="0" width="100%" id="template_footer" bgcolor="<?php echo esc_attr( $body ); ?>" style="background-color:<?php echo esc_attr( $body ); ?>;">
+										<tr>
+											<td valign="top" bgcolor="<?php echo esc_attr( $body ); ?>" style="background-color:<?php echo esc_attr( $body ); ?>;">
+												<table border="0" cellpadding="10" cellspacing="0" width="100%">
+													<tr>
+														<td colspan="2" valign="middle" id="credit" bgcolor="<?php echo esc_attr( $body ); ?>" style="background-color:<?php echo esc_attr( $body ); ?>;">
+															<?php
+															$email_footer_text = get_option( 'woocommerce_email_footer_text' );
+															if ( apply_filters( 'woocommerce_is_email_preview', false ) ) {
+																$text_transient    = get_transient( 'woocommerce_email_footer_text' );
+																$email_footer_text = false !== $text_transient ? $text_transient : $email_footer_text;
+															}
+															echo wp_kses_post(
+																wpautop(
+																	wptexturize(
+																		apply_filters( 'woocommerce_email_footer_text', $email_footer_text, $email )
+																	)
+																)
+															);
+															?>
+														</td>
+													</tr>
+												</table>
+											</td>
+										</tr>
+									</table>
+									<!-- End Footer -->
+								</td>
+							</tr>
+						</table>
+					</div>
+				</td>
+				<td><!-- Deliberately empty to support consistent sizing and layout across multiple email clients. --></td>
+			</tr>
+		</table>
+	</body>
+</html>
--- resources/views/woocommerce/emails/email-header.php
+++ resources/views/woocommerce/emails/email-header.php
@@ -4,8 +4,8 @@
  *
  * This template can be overridden by copying it to yourtheme/woocommerce/emails/email-header.php.
  *
- * @see     https://docs.woocommerce.com/document/template-structure/
- * @package WooCommerce/Templates/Emails
+ * @see     https://woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates\Emails
  * @version 10.0.0
  */
 
@@ -17,10 +17,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 $email_improvements_enabled = FeaturesUtil::feature_is_enabled( 'email_improvements' );
 $store_name                 = $store_name ?? get_bloginfo( 'name', 'display' );
-$logo_image_width           = absint( get_option( 'woocommerce_email_header_image_width', 120 ) );
-if ( $logo_image_width < 1 ) {
-	$logo_image_width = 120;
-}
+$bg                         = get_option( 'woocommerce_email_background_color', '#fcfcfc' );
+$body                       = get_option( 'woocommerce_email_body_background_color', '#ffffff' );
 
 ?>
 <!DOCTYPE html>
@@ -28,52 +26,56 @@ if ( $logo_image_width < 1 ) {
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo( 'charset' ); ?>" />
 		<meta content="width=device-width, initial-scale=1.0" name="viewport">
-		<meta name="color-scheme" content="light only">
-		<meta name="supported-color-schemes" content="light">
 		<title><?php echo esc_html( $store_name ); ?></title>
 	</head>
-	<body bgcolor="#f3f3f0" <?php echo is_rtl() ? 'rightmargin' : 'leftmargin'; ?>="0" marginwidth="0" topmargin="0" marginheight="0" offset="0" style="margin:0;padding:0;background-color:#f3f3f0 !important;">
-		<table width="100%" id="outer_wrapper" border="0" cellpadding="0" cellspacing="0" bgcolor="#f3f3f0" style="background-color:#f3f3f0 !important;margin:0;padding:0;width:100%;">
+	<body <?php echo is_rtl() ? 'rightmargin' : 'leftmargin'; ?>="0" marginwidth="0" topmargin="0" marginheight="0" offset="0" bgcolor="<?php echo esc_attr( $bg ); ?>" style="margin:0;padding:0;background-color:<?php echo esc_attr( $bg ); ?>;">
+		<table width="100%" id="outer_wrapper" border="0" cellpadding="0" cellspacing="0" bgcolor="<?php echo esc_attr( $bg ); ?>" style="background-color:<?php echo esc_attr( $bg ); ?>;">
 			<tr>
 				<td><!-- Deliberately empty to support consistent sizing and layout across multiple email clients. --></td>
-				<td width="600" bgcolor="#f3f3f0" style="background-color:#f3f3f0 !important;">
-					<div id="wrapper" dir="<?php echo is_rtl() ? 'rtl' : 'ltr'; ?>" style="background-color:#f3f3f0 !important;">
-						<table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" id="inner_wrapper" bgcolor="#f3f3f0" style="background-color:#f3f3f0 !important;">
+				<td width="600" bgcolor="<?php echo esc_attr( $bg ); ?>" style="background-color:<?php echo esc_attr( $bg ); ?>;">
+					<div id="wrapper" dir="<?php echo is_rtl() ? 'rtl' : 'ltr'; ?>">
+						<table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" id="inner_wrapper" bgcolor="<?php echo esc_attr( $email_improvements_enabled ? $body : $bg ); ?>" style="background-color:<?php echo esc_attr( $email_improvements_enabled ? $body : $bg ); ?>;">
 							<tr>
-								<td align="center" valign="top" bgcolor="#f3f3f0" style="background-color:#f3f3f0 !important;">
-									<table border="0" cellpadding="0" cellspacing="0" width="100%" id="template_container" bgcolor="#ffffff" style="background-color:#ffffff !important;border:1px solid #e5e5e5;border-radius:3px;">
-										<tr>
-											<td align="center" valign="top" bgcolor="#ffffff" style="background-color:#ffffff !important;">
-												<?php
-												$img = get_option( 'woocommerce_email_header_image' );
-												if ( apply_filters( 'woocommerce_is_email_preview', false ) ) {
-													$img_transient = get_transient( 'woocommerce_email_header_image' );
-													$img           = false !== $img_transient ? $img_transient : $img;
-												}
+								<td align="center" valign="top">
+									<?php
+									$img = get_option( 'woocommerce_email_header_image' );
+									if ( apply_filters( 'woocommerce_is_email_preview', false ) ) {
+										$img_transient = get_transient( 'woocommerce_email_header_image' );
+										$img           = false !== $img_transient ? $img_transient : $img;
+									}
 
-												if ( $img ) :
+									if ( $email_improvements_enabled ) :
+										?>
+										<table border="0" cellpadding="0" cellspacing="0" width="100%">
+											<tr>
+												<td id="template_header_image" bgcolor="<?php echo esc_attr( $body ); ?>" style="background-color:<?php echo esc_attr( $body ); ?>;">
+													<?php
+													if ( $img ) {
+														echo '<p style="margin-top:0;"><img src="' . esc_url( $img ) . '" alt="' . esc_attr( $store_name ) . '" /></p>';
+													} else {
+														echo '<p class="email-logo-text">' . esc_html( $store_name ) . '</p>';
+													}
 													?>
-													<table border="0" cellpadding="0" cellspacing="0" width="100%" bgcolor="#ffffff" style="background-color:#ffffff !important;">
-														<tr>
-															<td id="template_header_image" align="center" bgcolor="#ffffff" style="background-color:#ffffff !important;padding:24px 32px 0;text-align:center;">
-																<img src="<?php echo esc_url( $img ); ?>" alt="<?php echo esc_attr( $store_name ); ?>" width="<?php echo esc_attr( $logo_image_width ); ?>" style="display:block;margin:0 auto;max-width:<?php echo esc_attr( $logo_image_width ); ?>px;width:<?php echo esc_attr( $logo_image_width ); ?>px;height:auto;border:0;" />
-															</td>
-														</tr>
-													</table>
-												<?php elseif ( $email_improvements_enabled ) : ?>
-													<table border="0" cellpadding="0" cellspacing="0" width="100%" bgcolor="#ffffff" style="background-color:#ffffff !important;">
-														<tr>
-															<td id="template_header_image" align="center" bgcolor="#ffffff" style="background-color:#ffffff !important;padding:24px 32px 0;text-align:center;">
-																<p class="email-logo-text" style="margin:0;color:#53C999;font-size:18px;"><?php echo esc_html( $store_name ); ?></p>
-															</td>
-														</tr>
-													</table>
-												<?php endif; ?>
+												</td>
+											</tr>
+										</table>
+									<?php else : ?>
+										<div id="template_header_image">
+											<?php
+											if ( $img ) {
+												echo '<p style="margin-top:0;"><img src="' . esc_url( $img ) . '" alt="' . esc_attr( $store_name ) . '" /></p>';
+											}
+											?>
+										</div>
+									<?php endif; ?>
+									<table border="0" cellpadding="0" cellspacing="0" width="100%" id="template_container" bgcolor="<?php echo esc_attr( $body ); ?>" style="background-color:<?php echo esc_attr( $body ); ?>;">
+										<tr>
+											<td align="center" valign="top" bgcolor="<?php echo esc_attr( $body ); ?>" style="background-color:<?php echo esc_attr( $body ); ?>;">
 												<!-- Header -->
-												<table border="0" cellpadding="0" cellspacing="0" width="100%" id="template_header" bgcolor="#ffffff" style="background-color:#ffffff !important;">
+												<table border="0" cellpadding="0" cellspacing="0" width="100%" id="template_header" bgcolor="<?php echo esc_attr( $body ); ?>" style="background-color:<?php echo esc_attr( $body ); ?>;">
 													<tr>
-														<td id="header_wrapper" bgcolor="#ffffff" style="background-color:#ffffff !important;padding:<?php echo $email_improvements_enabled ? '20px 32px 0' : '36px 48px'; ?>;">
-															<h1 style="margin:0;word-break:normal;white-space:normal;color:#3c3c3c !important;"><?php echo esc_html( $email_heading ); ?></h1>
+														<td id="header_wrapper" bgcolor="<?php echo esc_attr( $body ); ?>" style="background-color:<?php echo esc_attr( $body ); ?>;">
+															<h1><?php echo esc_html( $email_heading ); ?></h1>
 														</td>
 													</tr>
 												</table>
@@ -81,13 +83,13 @@ if ( $logo_image_width < 1 ) {
 											</td>
 										</tr>
 										<tr>
-											<td align="center" valign="top" bgcolor="#ffffff" style="background-color:#ffffff !important;">
+											<td align="center" valign="top" bgcolor="<?php echo esc_attr( $body ); ?>" style="background-color:<?php echo esc_attr( $body ); ?>;">
 												<!-- Body -->
-												<table border="0" cellpadding="0" cellspacing="0" width="100%" id="template_body" bgcolor="#ffffff" style="background-color:#ffffff !important;">
+												<table border="0" cellpadding="0" cellspacing="0" width="100%" id="template_body" bgcolor="<?php echo esc_attr( $body ); ?>" style="background-color:<?php echo esc_attr( $body ); ?>;">
 													<tr>
-														<td valign="top" id="body_content" bgcolor="#ffffff" style="background-color:#ffffff !important;">
+														<td valign="top" id="body_content" bgcolor="<?php echo esc_attr( $body ); ?>" style="background-color:<?php echo esc_attr( $body ); ?>;">
 															<!-- Content -->
-															<table border="0" cellpadding="20" cellspacing="0" width="100%" bgcolor="#ffffff" style="background-color:#ffffff !important;">
+															<table border="0" cellpadding="20" cellspacing="0" width="100%">
 																<tr>
-																	<td valign="top" id="body_content_inner_cell" bgcolor="#ffffff" style="background-color:#ffffff !important;color:#575757 !important;">
-																		<div id="body_content_inner" style="color:#575757 !important;">
+																	<td valign="top" id="body_content_inner_cell" bgcolor="<?php echo esc_attr( $body ); ?>" style="background-color:<?php echo esc_attr( $body ); ?>;">
+																		<div id="body_content_inner">
